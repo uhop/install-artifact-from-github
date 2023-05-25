828a6a1f01765ea3c38b8b503fd832b576f23a53
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2023-05-25 11:36:23 -0500

    Example of a local script.

---
+++ scripts/example-save.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This is an example script to run save-to-github-cache.js locally, not in the context of Github Actions.
+# Copy and modify for your specific project.
+
+# IMPORTANT! Do not save the script with secrets in it in a publicly-available repository!
+
+# Set the repository:
+GITHUB_REPOSITORY=uhop/install-artifact-from-github
+
+# Set the release as a tag:
+GITHUB_REF=refs/tags/1.2.3-test
+
+# Use a personal token: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+# Set it to read/write code, all we need is to write an artifact into existing release.
+PERSONAL_TOKEN=github_...
+
+# Run save.
+npm run save-to-github
