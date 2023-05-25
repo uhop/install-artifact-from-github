aa7b16eab4ca16d4e0e680a9b2561ea532ece275
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2023-05-25 11:49:58 -0500

    Exported necessary variables.

--- scripts/example-save.sh
+++ scripts/example-save.sh
@@ -6,14 +6,14 @@
 # IMPORTANT! Do not save the script with secrets in it in a publicly-available repository!
 
 # Set the repository:
-GITHUB_REPOSITORY=uhop/install-artifact-from-github
+export GITHUB_REPOSITORY=uhop/install-artifact-from-github
 
 # Set the release as a tag:
-GITHUB_REF=refs/tags/1.2.3-test
+export GITHUB_REF=refs/tags/1.2.3-test
 
 # Use a personal token: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
-# Set it to read/write code, all we need is to write an artifact into existing release.
-PERSONAL_TOKEN=github_...
+# Set it to read/write code of the repository, all we need is to write an artifact into existing release.
+export PERSONAL_TOKEN=github_...
 
 # Run save.
 npm run save-to-github
